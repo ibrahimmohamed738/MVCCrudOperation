81dbc15ae1ca26bf746f808fd311fd6755b2cdc3
ibrahim <ibrahim.mohamed738@gmail.com>
2024-12-08 13:19:12 +0300

    Add project files.

---
+++ MVCCrudOperation/Controllers/UserController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVCCrudOperation.Data;
+using MVCCrudOperation.Models;
+
+namespace MVCCrudOperation.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly DataContext _context;
+
+        public UserController(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> List()
+        {
+            var users = await _context.Users.ToListAsync();
+            return View(users);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("List");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var user = _context.Users.Find(id);
+            return View(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(User user)
+        {
+            var dbUser = await _context.Users.FindAsync(user.Id);
+            if (dbUser != null)
+            {
+                dbUser.FirstName = user.FirstName;
+                dbUser.LastName = user.LastName;
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction("List");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var user = _context.Users.Find(id);
+            if (user != null)
+            {
+                _context.Remove(user);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction("List");
+        }
+    }
+}
---
+++ MVCCrudOperation/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCCrudOperation.Models;
+
+namespace MVCCrudOperation.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
